7ffa1847e5181c086d2a47237f3212080a93dc94
NIYIGENA Patrick <152195055+PattyWambere@users.noreply.github.com>
2026-02-03 14:28:59 +0200

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -20,7 +20,7 @@ app.use(cors());
 app.use(express.json());
 app.use(logger);
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));//
+app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerSpec));//
 
 // console.log("Swagger docs available at /api-docs", swaggerSpec);
 app.use("/api/categories/", categoryRoutes);
